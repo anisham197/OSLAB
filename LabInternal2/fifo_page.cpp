51df4418c3c9f6ded5c98c0ac276bcbb6a54aace
anisham197 <anisha.m.197@gmail.com>
2017-04-25 07:00:07 +0530

    Update fifo_page.cpp

--- LabInternal2/fifo_page.cpp
+++ LabInternal2/fifo_page.cpp
@@ -1,50 +1,48 @@
-#include <iostream>
-#include <queue>
-
+#include<iostream>
 using namespace std;
 
-int main(){
-        int n, r, pagefaults = 0, nextToReplace = 0;
-        cout<<"Enter the number of page frames\n";
-        cin >> n;
-        int frames[n];
+int main (){
+    int n, r, pageFaults = 0, nextToReplace = 0;
 
-        cout<<"Enter the number of page requests\n";
-        cin >> r;
+    cout << "Enter the number of page frames\n";
+    cin >> n;
 
-        for(int i = 0; i < n; i++){
-                frames[i] = -1;
+    int frames[n];
+    for(int i = 0; i < n; i++){
+            frames[i] = -1;
+    }
+
+    cout << "Enter the number of page requests\n";
+    cin >> r;
+
+    cout << "Enter the page requests\n";
+    for ( int i = 0; i < r; i++){
+        int request;
+        cin >> request;
+        int flag = 0;
+
+        cout << request << ": ";
+
+        for(int j = 0; j < n; j++){
+            if(frames[j] == request){
+                flag = 1;
+                for(int k = 0; k < n; k++)
+                    cout << frames[k] << " ";
+                cout << endl;
+            }
         }
 
-        cout<<"Enter the page requests\n";
-        for(int i = 0; i < r; i++){
+        if(flag) continue;
 
-            int request;
-            cin >> request;
-            bool flag = false;
+        pageFaults++;
+        frames[nextToReplace] = request;
+        nextToReplace = (nextToReplace + 1 )% n;
 
-            cout << request << ": ";
+        for(int k = 0; k < n; k++)
+            cout << frames[k] << " ";
+        cout << endl;
 
-            // Checks if request exists in page frames
-            for( int j = 0; j < n; j++){
-                if(frames[j] == request){
-                    flag = true;
-                    for(int k = 0; k < n; k++){
-                        cout << frames[k] << " ";
-                    }
-                    cout << endl;
-                }
-            }
-            if(flag) continue; // no page fault
+    }
+    cout <<"Number of Page Faults: " << pageFaults << endl;
 
-            frames[nextToReplace] = request;
-            pagefaults++;
-            nextToReplace = (nextToReplace + 1) % n;
-
-            for( int j = 0; j < n; j++){
-                    cout << frames[j] << " ";
-            }
-            cout<<endl;
-        }
-        cout<<"Number of Page Faults: "<< pagefaults << endl;
 }
